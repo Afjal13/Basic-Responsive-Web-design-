e4cd97b431da12453d9da07de6a823bca8d16112
Afjal Hossain <32141447+Afjal13@users.noreply.github.com>
2020-03-11 03:07:28 +0600

    Self Practices (HTML, CSS, JS)

---
+++ show_hide.js
@@ -0,0 +1,22 @@
+var divs = ["top_bar", "left_sidebar"];
+    var visibleDivId = null;
+    function toggleVisibility(divId) {
+      if(visibleDivId === divId) {
+        visibleDivId = null;
+      } else {
+        visibleDivId = divId;
+      }
+      hideNonVisibleDivs();
+    }
+    function hideNonVisibleDivs() {
+      var i, divId, div;
+      for(i = 0; i < divs.length; i++) {
+        divId = divs[i];
+        div = document.getElementById(divId);
+        if(visibleDivId === divId) {
+          div.style.display = "block";
+        } else {
+          div.style.display = "none";
+        }
+      }
+    }
